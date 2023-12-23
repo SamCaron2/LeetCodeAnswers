a12e5054e5199f75bfe03a6e62a38f7a9168e095
Samuel Caron <112727994+SamCaron2@users.noreply.github.com>
2023-12-23 03:38:23 -0600

    Time: 22 ms (12.60%), Space: 13.7 MB (6.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,28 @@
 class Solution(object):
     def isValid(self, s):
-        stack = []
+        stack = [] #Create an empty stack
         
         for i in s:
             if i == "(" or i == "[" or i == "{":
-                stack.append(i)
-            elif(len(stack)):
-                topOfStack = stack.pop()
+                stack.append(i) #if index i is any of the three open brackets append to created stack
+                
+            elif(len(stack)): #if it is closing brackets check to see if len > 0
+                
+                topOfStack = stack.pop() #get the char at the top of the stack
+                
+                #if the top of the stack does not match up with index i return false
                 if topOfStack == "(" and i != ")":
                     return False
-                elif topOfStack == "[" and i != "]":
+                elif topOfStack == "[" and i != "]": 
                     return False
                 elif topOfStack == "{" and i != "}":
                     return False
                 
+           # If both conditions fail return false    
             else:
                 return False
+            
+        #if there are any remaining characters in the stack return false
         if(len(stack)):
             return False
         else:
